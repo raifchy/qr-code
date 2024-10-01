19498491f9c9b3dafcf66df7a9e766bf2cc99812
raif chy <raif.personal03@gmail.com>
2024-10-01 11:05:56 +0600

    image moved to public

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,11 @@ export default function App() {
     <>
       <main className=" bg-slate-200 h-screen flex flex-col justify-center">
         <div className="w-72 h-fit bg-white p-3  mx-auto rounded-2xl shadow-xl">
-          <img src="../images/image-qr-code.png" className=" rounded-lg" alt="" />
+          <img
+            src="../public/image-qr-code.png"
+            className=" rounded-lg"
+            alt=""
+          />
           <div className="p-4 text-center">
             <h1 className="mb-3 leading-6 font-outfit font-outline font-bold text-slate-900 text-lg">
               Improve your front-end skills by building projects
